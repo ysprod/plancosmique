690ee7ab4c51bd8397d5e8830607e377a8af7215
ysprod <yayasidibeproduction@gmail.com>
2026-01-15 22:50:06 +0000

    fix: Ajout de min-h-screen pour corriger l'affichage de la page de déconnexion

--- components/auth/LogoutPageClient.tsx
+++ components/auth/LogoutPageClient.tsx
@@ -20,7 +20,7 @@ export default function LogoutPageClient() {
   })();
 
   return (
-    <div className=" bg-gradient-to-br from-slate-900 via-purple-900 to-violet-900 relative overflow-hidden flex items-center justify-center p-3 sm:p-6">
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-violet-900 relative overflow-hidden flex items-center justify-center p-3 sm:p-6">
       <div className="absolute inset-0 overflow-hidden">
         <AnimatedBackground />
       </div>
